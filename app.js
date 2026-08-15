c2dc12f566d5c2abc2e3dc3fdf4d62b640d0ebab
moritsugusm-collab <moritsugusm@gmail.com>
2026-08-15 12:03:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const TITLES = [
     ],
 
     image1:
-      "images/big-x.jpg",
+      "images/empty-bookmark.png",
 
     image2:
       "images/smiley.jpg"
@@ -44,7 +44,7 @@ const TITLES = [
     ],
 
     image1:
-      "images/dune-not-read.jpg",
+      "images/empty-bookmark.png",
 
     image2:
       "images/dune-read.jpg"
@@ -61,7 +61,7 @@ const TITLES = [
     ],
 
     image1:
-      "images/lotr-not-read.jpg",
+      "images/empty-bookmark.png",
 
     image2:
       "images/lotr-read.jpg"
